42182fa2582ea178fba6aa0c4d847bfa528afa26
detanaded <48308236+detanaded@users.noreply.github.com>
2019-06-03 13:43:23 -0600

    Inital Setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express'),
+      massive = require('massive'),
+      session = require('express-session')
+const{SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env
+
+const app = express()
+app.use(express.json())
+
+app.use(session({
+  secret: SESSION_SECRET,
+  saveUninitialized: false,
+  resave: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60
+  }
+}))
+
+
+massive(CONNECTION_STRING).then((database) =>{
+  app.set('db', database)
+  console.log('Pepebase Set!')
+})
+
+
+app.listen(SERVER_PORT, () => {console.log(`Pepe Memin on ${SERVER_PORT}!`)})
